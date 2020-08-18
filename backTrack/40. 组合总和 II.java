9dde1a05174500200db7714eb9461a54dd76529a
dnsritch <dnsritch@gmail.com>
2020-08-18 23:48:36 +0800

    组合总和

--- backTrack/40. 组合总和 II.java
+++ backTrack/40. 组合总和 II.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 class Solution {
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> resList = new ArrayList<>();
-
         Arrays.sort(candidates);
+
         backTrack(0, target, candidates, new ArrayList<>(), resList);
 
         return resList;
@@ -20,11 +20,15 @@ class Solution {
         }
         for (int i = first; i < array.length; i++) {
             int temp = array[i];
+            // 大剪枝
             if (temp > target) {
                 return;
+                // 小剪枝
+            } else if (i > first && array[i - 1] == array[i]) {
+                continue;
             } else {
                 list.add(temp);
-                backTrack(i, target - temp, array, list, resList);
+                backTrack(i + 1, target - temp, array, list, resList);
                 list.remove(list.size() - 1);
             }
         }
