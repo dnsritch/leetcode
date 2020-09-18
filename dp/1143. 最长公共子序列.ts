014861b5c2cfa18352ac435461df59e1fc66d3f8
dnsritch <dnsritch@gmail.com>
2020-09-18 09:58:54 +0800

    dp 练习

--- dp/1143. 最长公共子序列.ts
+++ dp/1143. 最长公共子序列.ts
@@ -1,5 +1,21 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-  if (text1 === text2) {
-    return text1.length
+  const m = text1.length
+  const n = text2.length
+
+  const list = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0))
+
+  //注意 i,j 初值
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        list[i][j] = list[i - 1][j - 1] + 1
+      } else {
+        list[i][j] = Math.max(list[i - 1][j], list[i][j - 1])
+      }
+    }
   }
+
+  return list[m][n]
 }
--- dp/152. 乘积最大子数组.java
+++ dp/152. 乘积最大子数组.java
@@ -2,8 +2,9 @@ class Solution {
     public int maxProduct(int[] nums) {
         int max = 0;
         int length = nums.length;
-        int maxArr = new int[length];
-        int minArr = new int[length];
+
+        int[] maxArr = new int[length];
+        int[] minArr = new int[length];
 
         for (int i = 1; i < nums.length; i++) {
             maxArr[i] = Math.max(a, b);
